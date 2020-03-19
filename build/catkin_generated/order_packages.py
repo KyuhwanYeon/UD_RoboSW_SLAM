8098ae12afb3b38fb994cd15c696aafb59a85a26
KyuhwanYeon <KyuhwanYeon@gmail.com>
2020-03-19 20:39:04 +0900

    Remove file from previous project , and add database file

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/khyeon/workspace/019_MapMyWorld/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/khyeon/workspace/019_MapMyWorld/devel;/home/khyeon/workspace/010_Localization/devel;/opt/ros/kinetic".split(';') if "/home/khyeon/workspace/019_MapMyWorld/devel;/home/khyeon/workspace/010_Localization/devel;/opt/ros/kinetic" != "" else []
--- build/my_robot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/khyeon/workspace/019_MapMyWorld/src/my_robot
-# Build directory: /home/khyeon/workspace/019_MapMyWorld/build/my_robot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
