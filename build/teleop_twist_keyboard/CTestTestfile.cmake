fde98dac0d7c5773e330c24443686fce49609a48
KyuhwanYeon <KyuhwanYeon@gmail.com>
2020-03-21 00:35:30 +0900

    Add database, Modify mapping.launch for parameters of RTAB MAPS

--- build/teleop_twist_keyboard/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/khyeon/workspace/019_MapMyWorld/src/teleop_twist_keyboard
-# Build directory: /home/khyeon/workspace/019_MapMyWorld/build/teleop_twist_keyboard
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
